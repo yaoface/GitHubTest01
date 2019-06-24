f63850302b849355593bbbe0fabf3cd0fba6e654
yaoface <1500474276@qq.com>
2019-06-24 21:28:30 +0800

    update01

--- src/main/java/com/atguigu/HelloGit.java
+++ src/main/java/com/atguigu/HelloGit.java
@@ -3,5 +3,6 @@ package com.atguigu;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("AAAAAAAAAAAAA");
+        System.out.println("BBBBBBBBBBBBB");
     }
 }
